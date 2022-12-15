4b493a659475c3c68fe68372b767931be5901022
Asim Ali Murtaza <mo.asim.murtaza@gmail.com>
2022-12-15 19:41:43 +0500

    These are PF Lab Week 02 projects.

---
+++ projectHeader.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main ()
+{
+cout << ("*************************") << endl;
+cout << ("*   ANSARI OPTICIANS    *") << endl;
+cout << ("*      ASSOCIATION      *") << endl;
+cout << ("*                       *") << endl;
+cout << ("*                       *") << endl;
+cout << ("*                       *") << endl;
+cout << ("*                       *") << endl;
+cout << ("*                       *") << endl;
+cout << ("*************************") << endl;
+return 0;
+}
